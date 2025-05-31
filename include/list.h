b3d2da7a7ae0a5c306898e3dadada97614b2a80e
ArtemChurilov <st130184@student.spbu.ru>
2025-05-31 23:56:03 +0300

    add removing first element, also add out_of_range error

--- include/list.h
+++ include/list.h
@@ -15,23 +15,31 @@ private:
         Node(const T& val) : value(val), next(nullptr) {}
     };
     Node* head;
+    size_t size;
 public:
-    SkipList() : head(nullptr) {}
+    SkipList() : head(nullptr),size(0) {}
     ~SkipList()
     {
-        Node* current = head;
-        while (current)
-        {
-            Node* next = current->next;
-            delete current;
-            current = next;
-        }
+        clear();
     }
     void insert(const T& value)
     {
         Node* newNode = new Node(value);
         newNode->next = head;
         head = newNode;
+        size++;
+    }
+    void pop_front() 
+    {
+        if (!head) 
+        {
+            throw std::out_of_range("List is empty");
+        }
+        
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        size--;
     }
     void print() const
     {
@@ -42,6 +50,13 @@ public:
             current = current->next;
         }
         std::cout << "\n";
+    }
+    void clear()
+    {
+        while (head) 
+        {
+            pop_front();
+        }
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,22 @@
 int main()
 {
     SkipList<int> list;
-
     list.insert(1);
     list.insert(2);
     list.insert(3);
-
+    list.insert(6);
     std::cout << "List contents: ";
     list.print();
+    list.clear();
 
+    list.pop_front();
+    std::cout << "List contents: ";
+    list.print();
+    list.insert(42);
+    list.insert(24);
+    list.pop_front();
+    
+    std::cout << "List contents: ";
+    list.print();
     return 0;
 }
